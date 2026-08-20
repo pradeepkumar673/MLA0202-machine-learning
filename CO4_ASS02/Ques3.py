f9e0c1a873cf31c1d4548cec804d3b019aa52e57
pradeep_the_terrible <pradeepcastleofficial@gmail.com>
2026-08-20 13:54:57 +0530

    333

--- CO4_ASS02/Ques3.py
+++ CO4_ASS02/Ques3.py
@@ -1,159 +1,34 @@
 import sklearn_crfsuite
 from sklearn_crfsuite import metrics
 
-train_data = [
-    [
-        ("John", "B-CUSTOMER"),
-        ("ordered", "O"),
-        ("a", "O"),
-        ("Laptop", "B-PRODUCT"),
-        ("with", "O"),
-        ("order", "O"),
-        ("12345", "B-ORDER"),
-        ("and", "O"),
-        ("reported", "O"),
-        ("a", "O"),
-        ("broken", "B-ISSUE"),
-        ("screen", "I-ISSUE")
-    ],
-    [
-        ("Priya", "B-CUSTOMER"),
-        ("bought", "O"),
-        ("Phone", "B-PRODUCT"),
-        ("with", "O"),
-        ("order", "O"),
-        ("56789", "B-ORDER"),
-        ("and", "O"),
-        ("said", "O"),
-        ("battery", "B-ISSUE"),
-        ("is", "I-ISSUE"),
-        ("dead", "I-ISSUE")
-    ],
-    [
-        ("Rahul", "B-CUSTOMER"),
-        ("received", "O"),
-        ("Tablet", "B-PRODUCT"),
-        ("under", "O"),
-        ("order", "O"),
-        ("24680", "B-ORDER"),
-        ("and", "O"),
-        ("reported", "O"),
-        ("damaged", "B-ISSUE"),
-        ("screen", "I-ISSUE")
-    ],
-    [
-        ("Arun", "B-CUSTOMER"),
-        ("purchased", "O"),
-        ("Monitor", "B-PRODUCT"),
-        ("for", "O"),
-        ("order", "O"),
-        ("67890", "B-ORDER"),
-        ("and", "O"),
-        ("reported", "O"),
-        ("display", "B-ISSUE"),
-        ("problem", "I-ISSUE")
-    ]
+train = [
+    [("John","B-CUSTOMER"),("bought","O"),("Laptop","B-PRODUCT"),("1234","B-ORDER")],
+    [("Priya","B-CUSTOMER"),("bought","O"),("Phone","B-PRODUCT"),("5678","B-ORDER")]
 ]
 
-test_data = [
-    [
-        ("Anita", "B-CUSTOMER"),
-        ("ordered", "O"),
-        ("Laptop", "B-PRODUCT"),
-        ("under", "O"),
-        ("order", "O"),
-        ("13579", "B-ORDER"),
-        ("and", "O"),
-        ("reported", "O"),
-        ("battery", "B-ISSUE"),
-        ("problem", "I-ISSUE")
-    ]
+test = [
+    [("Rahul","B-CUSTOMER"),("bought","O"),("Tablet","B-PRODUCT"),("9999","B-ORDER")]
 ]
 
-def word_features(sentence, index):
-    word = sentence[index][0]
+def features(s):
+    return [[
+        "word=" + w,
+        "lower=" + w.lower(),
+        "digit=" + str(w.isdigit())
+    ] for w, l in s]
 
-    features = {
-        "word": word,
-        "lowercase": word.lower(),
-        "uppercase": word.isupper(),
-        "digit": word.isdigit(),
-        "length": len(word),
-        "prefix": word[:2],
-        "suffix": word[-2:]
-    }
+X = [features(s) for s in train]
+y = [[l for w, l in s] for s in train]
 
-    if index > 0:
-        previous_word = sentence[index - 1][0]
-        features["previous_word"] = previous_word.lower()
-    else:
-        features["beginning"] = True
+Xt = [features(s) for s in test]
+yt = [[l for w, l in s] for s in test]
 
-    if index < len(sentence) - 1:
-        next_word = sentence[index + 1][0]
-        features["next_word"] = next_word.lower()
-    else:
-        features["ending"] = True
+model = sklearn_crfsuite.CRF(max_iterations=100)
+model.fit(X, y)
 
-    return features
+pred = model.predict(Xt)
 
-def get_features(sentence):
-    return [
-        word_features(sentence, i)
-        for i in range(len(sentence))
-    ]
-
-X_train = [
-    get_features(sentence)
-    for sentence in train_data
-]
-
-y_train = [
-    [label for word, label in sentence]
-    for sentence in train_data
-]
-
-X_test = [
-    get_features(sentence)
-    for sentence in test_data
-]
-
-y_test = [
-    [label for word, label in sentence]
-    for sentence in test_data
-]
-
-model = sklearn_crfsuite.CRF(
-    algorithm="lbfgs",
-    max_iterations=100,
-    all_possible_transitions=True
-)
-
-model.fit(X_train, y_train)
-
-predictions = model.predict(X_test)
-
-print("CRF Named Entity Recognition")
-print("----------------------------")
-
-print("\nPredicted Entities:")
-
-for i in range(len(test_data[0])):
-    word = test_data[0][i][0]
-    label = predictions[0][i]
+for word, label in zip([w for w, l in test[0]], pred[0]):
     print(word, "->", label)
 
-accuracy = metrics.flat_accuracy_score(
-    y_test,
-    predictions
-)
-
-print("\nAccuracy:", round(accuracy * 100, 2), "%")
-
-print("\nClassification Report:")
-print(
-    metrics.flat_classification_report(
-        y_test,
-        predictions
-    )
-)
+print("Accuracy:", metrics.flat_accuracy_score(yt, pred))
